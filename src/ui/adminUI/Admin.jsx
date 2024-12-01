ce30be0cfc917a56db4ada61d38509927352ee3c
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2024-11-30 21:24:45 -0500

    aa

--- src/ui/adminUI/Admin.jsx
+++ src/ui/adminUI/Admin.jsx
@@ -158,157 +158,166 @@ const Admin = () => {
   };
   
   return (
-    <div className="flex flex-col h-screen bg-blue-100 text-gray-900 min-h-screen">
+    <div className="flex h-screen bg-blue-100 text-gray-900 min-h-screen">
       <SidebarAdmin />
-  
+
       {/* Mostrar Spinner mientras carga */}
       {loading && <Spinner />}
-  
-      <div className="flex-1 flex flex-col items-center justify-start space-y-6 px-4 sm:px-8 md:px-16 mt-10 w-full overflow-hidden">
-        <h2 className="text-xl sm:text-2xl font-bold text-center">
-          Gestión de Dispositivos y Familiares
-        </h2>
-  
+
+      <div className="flex-1 flex flex-col items-center justify-start space-y-8 px-4 sm:px-8 md:px-16 mt-20">
+        <h2 className="text-3xl font-bold text-center">Gestión de Dispositivos y Familiares</h2>
+
         {/* Contenedor de Modo Vigilancia */}
-        <div className="w-full max-w-lg bg-gray-100 p-4 sm:p-6 rounded-lg shadow-md">
-          <h3 className="text-lg sm:text-xl font-semibold">Modo Vigilancia</h3>
+        <div className="w-full max-w-3xl bg-gray-100 p-6 rounded-lg shadow-md space-y-4">
+          <h3 className="text-xl font-semibold">Modo Vigilancia</h3>
           <button
             onClick={toggleVigilanceMode}
-            className={`flex items-center justify-center w-full h-12 rounded-lg text-white font-semibold transition-all duration-300 ease-in-out ${
-              isVigilanceModeOn ? "bg-green-500" : "bg-red-500"
-            } ${loading ? "opacity-50 cursor-not-allowed" : ""}`}
+            className={`flex items-center justify-center w-full sm:w-60 h-12 rounded-lg text-white font-semibold transition-all duration-300 ease-in-out ${
+              isVigilanceModeOn ? 'bg-green-500' : 'bg-red-500'
+            } ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
             disabled={loading}
           >
-            {isVigilanceModeOn
-              ? "Modo Vigilancia Encendido"
-              : "Modo Vigilancia Apagado"}
+            <span className="mr-2">
+              {isVigilanceModeOn ? 'Modo Vigilancia Encendido' : 'Modo Vigilancia Apagado'}
+            </span>
           </button>
         </div>
-  
+
         {/* Contenedor de Agregar Familiar */}
-        <div className="w-full max-w-lg bg-gray-100 p-4 sm:p-6 rounded-lg shadow-md">
-          <h3 className="text-lg sm:text-xl font-semibold">Agregar Familiar</h3>
+        <div className="w-full max-w-3xl bg-gray-100 p-6 rounded-lg shadow-md space-y-4">
+          <h3 className="text-xl font-semibold">Agregar Familiar</h3>
           <div className="space-y-4">
-            {/* Campos flexibles */}
+
+            {/* Contenedor con flexbox para campos */}
             <div className="flex flex-wrap gap-4">
-              <div className="flex-1 w-full">
+              {/* Campo para el nombre de usuario */}
+              <div className="flex-1 min-w-[250px]">
                 <input
                   type="text"
                   name="username"
                   value={familyData.username}
                   onChange={handleFamilyInputChange}
                   placeholder="Nombre de Usuario"
-                  className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:ring-2 focus:ring-green-500"
+                  className="w-full px-4 py-2 bg-gray-200 text-gray-900 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
                 />
                 {formErrors.username && (
                   <p className="text-red-500 text-sm">{formErrors.username}</p>
                 )}
               </div>
-  
-              <div className="flex-1 w-full">
+
+              {/* Campo para los nombres */}
+              <div className="flex-1 min-w-[250px]">
                 <input
                   type="text"
                   name="nombres"
                   value={familyData.nombres}
                   onChange={handleFamilyInputChange}
                   placeholder="Nombres"
-                  className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:ring-2 focus:ring-green-500"
+                  className="w-full px-4 py-2 bg-gray-200 text-gray-900 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
                   onInput={(e) => {
-                    e.target.value = e.target.value.replace(/[^A-Za-z\s]/g, "");
+                    e.target.value = e.target.value.replace(/[^A-Za-z\s]/g, '');
                   }}
+                  title="El campo 'Nombres' solo debe contener letras y espacios."
                 />
                 {formErrors.nombres && (
                   <p className="text-red-500 text-sm">{formErrors.nombres}</p>
                 )}
               </div>
-  
-              <div className="flex-1 w-full">
+
+              {/* Campo para los apellidos */}
+              <div className="flex-1 min-w-[250px]">
                 <input
                   type="text"
                   name="apellidos"
                   value={familyData.apellidos}
                   onChange={handleFamilyInputChange}
                   placeholder="Apellidos"
-                  className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:ring-2 focus:ring-green-500"
+                  className="w-full px-4 py-2 bg-gray-200 text-gray-900 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
                   onInput={(e) => {
-                    e.target.value = e.target.value.replace(/[^A-Za-z\s]/g, "");
+                    e.target.value = e.target.value.replace(/[^A-Za-z\s]/g, '');
                   }}
+                  title="El campo 'Apellidos' solo debe contener letras y espacios."
                 />
                 {formErrors.apellidos && (
                   <p className="text-red-500 text-sm">{formErrors.apellidos}</p>
                 )}
               </div>
-  
-              <div className="flex-1 w-full">
+
+              {/* Campo para el correo */}
+              <div className="flex-1 min-w-[250px]">
                 <input
                   type="email"
                   name="correo"
                   value={familyData.correo}
                   onChange={handleFamilyInputChange}
                   placeholder="Correo Electrónico"
-                  className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:ring-2 focus:ring-green-500"
+                  className="w-full px-4 py-2 bg-gray-200 text-gray-900 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
                 />
                 {formErrors.correo && (
                   <p className="text-red-500 text-sm">{formErrors.correo}</p>
                 )}
               </div>
-  
-              <div className="flex-1 w-full">
+
+              {/* Campo para el DNI */}
+              <div className="flex-1 min-w-[250px]">
                 <input
                   type="text"
                   name="dni"
                   value={familyData.dni}
                   onChange={handleFamilyInputChange}
                   placeholder="DNI"
+                  className="w-full px-4 py-2 bg-gray-200 text-gray-900 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
                   maxLength={8}
-                  className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:ring-2 focus:ring-green-500"
+                  pattern="^\d{8}$"
                   onInput={(e) => {
-                    e.target.value = e.target.value.replace(/\D/g, "").slice(0, 8);
+                    e.target.value = e.target.value.replace(/\D/g, '').slice(0, 8);
                   }}
                 />
                 {formErrors.dni && (
                   <p className="text-red-500 text-sm">{formErrors.dni}</p>
                 )}
               </div>
-  
-              <div className="flex-1 w-full">
+            </div>
+
+            <div className="flex flex-wrap gap-4">
+              {/* Campo para la contraseña */}
+              <div className="flex-1 min-w-[250px]">
                 <input
                   type="password"
                   name="password"
                   value={familyData.password}
                   onChange={handleFamilyInputChange}
                   placeholder="Contraseña"
-                  className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:ring-2 focus:ring-green-500"
+                  className="w-full px-4 py-2 bg-gray-200 text-gray-900 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
                 />
                 {formErrors.password && (
                   <p className="text-red-500 text-sm">{formErrors.password}</p>
                 )}
               </div>
-  
-              <div className="flex-1 w-full">
+
+              {/* Campo para repetir la contraseña */}
+              <div className="flex-1 min-w-[250px]">
                 <input
                   type="password"
                   name="repeatPassword"
                   value={familyData.repeatPassword}
                   onChange={handleFamilyInputChange}
                   placeholder="Repetir Contraseña"
-                  className="w-full px-4 py-2 bg-gray-200 rounded-lg focus:ring-2 focus:ring-green-500"
+                  className="w-full px-4 py-2 bg-gray-200 text-gray-900 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
                 />
                 {formErrors.repeatPassword && (
-                  <p className="text-red-500 text-sm">
-                    {formErrors.repeatPassword}
-                  </p>
+                  <p className="text-red-500 text-sm">{formErrors.repeatPassword}</p>
                 )}
               </div>
             </div>
-  
-            <button
-              onClick={addFamilyMember}
-              className="w-full bg-green-500 hover:bg-green-400 text-white font-semibold py-2 rounded transition-colors"
-            >
-              Agregar Familiar
-            </button>
           </div>
+
+          <button
+            onClick={addFamilyMember}
+            className="w-full bg-green-500 hover:bg-green-400 text-white font-semibold py-2 px-6 rounded transition-colors"
+          >
+            Agregar Familiar
+          </button>
         </div>
       </div>
     </div>
